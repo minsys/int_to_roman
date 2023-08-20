a81a9bfb08db82ab34f854700e090e2609605fca
Hugo Claesen <hugo_claesen@hotmail.com>
2023-08-20 22:46:28 +0200

    Complete implementation

--- src/int_to_roman.rs
+++ src/int_to_roman.rs
@@ -9,20 +9,36 @@ pub fn convert(i: i64) -> Result<String, &'static str> {
     let fiftees : i64 = (i - thousands * 1000 - fivehundreds * 500 - hundreds * 100) / 50;
     let tens : i64 = (i - thousands * 1000 - fivehundreds * 500 - hundreds * 100 - fiftees * 50) / 10;
     let fives : i64 = (i - thousands * 1000 - fivehundreds * 500 - hundreds * 100 - fiftees * 50 - tens * 10) / 5;
-    let ones : i64 = (i - thousands * 1000 - fivehundreds * 500 - hundreds * 100 - fiftees * 50 - tens * 10 - fives * 5);
+    let ones : i64 = i - thousands * 1000 - fivehundreds * 500 - hundreds * 100 - fiftees * 50 - tens * 10 - fives * 5;
 
     let mut roman_number = String::new();
 
-    for _ in 1..thousands {
+    for _ in 0..thousands {
         roman_number.push_str("M");
     }
-    append_order(fivehundreds, 'D', hundreds, 'C', 'M', roman_number);
-    append_order(fiftees, 'L', tens, 'X', 'C', roman_number);
-    append_order(fives, 'V', ones, 'I', 'X', roman_number);
+    append_order(fivehundreds, "D", hundreds, "C", "M", &mut roman_number);
+    append_order(fiftees, "L", tens, "X", "C", &mut roman_number);
+    append_order(fives, "V", ones, "I", "X", &mut roman_number);
 
     Ok(roman_number)
 }
 
-fn append_order( fives : i64, five_symbol : str&, ones : i64, oneSymbol : str&, tenSymbol : str&, roman_number : mut String&) {
+fn append_order( fives : i64, five_symbol : &str, ones : i64, one_symbol : &str, ten_symbol : &str, roman_number : &mut String) {
+    for _ in 0..fives {
+        roman_number.push_str(five_symbol);
+    }
 
+    if ones == 4 && fives == 1 {
+        roman_number.pop();
+        roman_number.push_str(one_symbol);
+        roman_number.push_str(ten_symbol);
+    }
+    else if ones == 4 {
+        roman_number.push_str(one_symbol);
+        roman_number.push_str(five_symbol);
+    } else {
+        for _ in 0..ones {
+            roman_number.push_str(one_symbol);
+        }
+    }
 }
